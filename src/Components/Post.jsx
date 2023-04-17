33831d287a33d3b3f3ad13402f1fa77e04ce6cf3
Thiago <will77.tj@gmail.com>
2023-04-17 10:56:09 -0300

    Upload

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -5,7 +5,7 @@ export function Post() {
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img className={styles.avatar} src='https://instagram.fpnz4-1.fna.fbcdn.net/v/t51.2885-15/307221793_619081323185693_7923157717664767882_n.jpg?stp=dst-jpg_e35_p720x720&_nc_ht=instagram.fpnz4-1.fna.fbcdn.net&_nc_cat=105&_nc_ohc=I8yGdz7jidQAX_mVqNY&edm=ACWDqb8BAAAA&ccb=7-5&ig_cache_key=MjkyOTQ1MTU2NTIwMzA2NDAzNA%3D%3D.2-ccb7-5&oh=00_AfDYlvg5EidJAjB3rXimqYnebV2aXk-5apwFYQhfopXEag&oe=6442F0B3&_nc_sid=1527a3' />
+                    <img className={styles.avatar} src="./cadura.jpg" />
                     <div className={styles.authorInfo}>
                         <strong>Filipe Cadura</strong>
                         <span>Estressado de Plantão</span>
